2713e24521193cc458f0d03b1a60bda54a929aa8
trevonerd <marco.trevisani81@gmail.com>
2026-09-18 16:35:53 +0200

    chore(release): prepare 4.0.0-beta.33

--- tests/manifest-permissions.test.ts
+++ tests/manifest-permissions.test.ts
@@ -58,8 +58,8 @@ describe('manifest permissions', () => {
 
     expect(releaseVersion).toEqual({
       channel: 'beta',
-      manifestVersion: '3.99.0.32',
-      versionName: '4.0.0-beta.32',
+      manifestVersion: '3.99.0.33',
+      versionName: '4.0.0-beta.33',
     });
   });
 
